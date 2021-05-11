bed4098f65d870a4d0b89f0d430a38b7465db6b2
ayush <ayush6240@gmail.com>
2021-05-11 16:44:31 +0530

    initial frontend commit

--- Reddit-Backend/src/main/java/com/example/reddit/RedditBackendApplication.java
+++ Reddit-Backend/src/main/java/com/example/reddit/RedditBackendApplication.java
@@ -2,10 +2,14 @@ package com.example.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.example.reddit.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditBackendApplication {
 
 	public static void main(String[] args) {
